eebde81307404252f19d007ea654e39efa7dbf16
xbello <xbello@gmail.com>
2013-12-05 10:50:59 +0100

    Filter users to staff members
    In a system with lots of users, the default admin form is filled with regular users. The simplicity of this app make it easiest to hardcode the filtering than to create a new app that subclasses it.

--- issuetracker/admin.py
+++ issuetracker/admin.py
@@ -1,8 +1,19 @@
+from django.contrib.auth.models import User
+from django import forms
 from django.contrib import admin
 from issuetracker.models import Ticket
 
+class TicketAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TicketAdminForm, self).__init__(*args, **kwargs)
+        # Filter down submitters and assigned_to to just staff members
+        staff_members = User.objects.filter(is_staff=True)
+        self.fields["submitter"].queryset = staff_members
+        self.fields["assigned_to"].queryset = staff_members
 
 class TicketAdmin(admin.ModelAdmin):
+    form = TicketAdminForm
+    
     list_display = ('title', 'status', 'priority', 'submitter',
         'submitted_date', 'modified_date')
     list_filter = ('priority', 'status', 'submitted_date')
